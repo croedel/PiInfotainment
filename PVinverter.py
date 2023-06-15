cfb01eb4464267f64989b6f0f9f9fcd6d12a6da9
Christian Rödel <christian@roedel.info>
2023-06-15 22:07:58 +0200

    changed to daysummary

--- PVinverter.py
+++ PVinverter.py
@@ -53,31 +53,34 @@ def get_PV_device_data():
     now = time.localtime() # "now" = time of data refresh
     pvdata["dt"] = { "value": time.strftime("%d.%m.%Y %H:%M:%S", now), "unit": "" }
 
-    api = MTECapi.MTECapi()
-    data = api.query_station_data(station_id)
-    pvdata["day_production"] = data["todayEnergy"]              # Energy produced by the PV today
-    pvdata["month_production"] = data["monthEnergy"]            # Energy produced by the PV this month
-    pvdata["year_production"] = data["yearEnergy"]              # Energy produced by the PV this year
-    pvdata["total_production"] = data["totalEnergy"]            # Energy produced by the PV in total
-    pvdata["current_PV"] = format_data(data["PV"])              # Current flow from PV
-    pvdata["current_grid"] = format_data( data["grid"] )        # Current flow from/to grid
-    pvdata["current_battery"] = format_data( data["battery"] )  # Current flow from/to battery
-    pvdata["current_battery_SOC"] = { "value": data["battery"]["SOC"], "unit": "%" }   # Current battery SOC
-    pvdata["current_load"] = format_data( data["load"])          # Current consumed energy
-    pvdata["grid_interrupt"] = { "value": data["lackMaster"], "unit": "" }  # Grid interrup flag
+    try:
+        api = MTECapi.MTECapi()
+        data = api.query_station_data(station_id)
+        pvdata["day_production"] = data["todayEnergy"]              # Energy produced by the PV today
+        pvdata["month_production"] = data["monthEnergy"]            # Energy produced by the PV this month
+        pvdata["year_production"] = data["yearEnergy"]              # Energy produced by the PV this year
+        pvdata["total_production"] = data["totalEnergy"]            # Energy produced by the PV in total
+        pvdata["current_PV"] = format_data(data["PV"])              # Current flow from PV
+        pvdata["current_grid"] = format_data( data["grid"] )        # Current flow from/to grid
+        pvdata["current_battery"] = format_data( data["battery"] )  # Current flow from/to battery
+        pvdata["current_battery_SOC"] = { "value": data["battery"]["SOC"], "unit": "%" }   # Current battery SOC
+        pvdata["current_load"] = format_data( data["load"])          # Current consumed energy
+        pvdata["grid_interrupt"] = { "value": data["lackMaster"], "unit": "" }  # Grid interrup flag
 
-    data = api.query_usage_data(station_id, "day")
-    pvdata["day_grid_load"] = data["day_grid_load"]     # Today's energy loaded from grid
-    pvdata["day_grid_feed"] = data["day_grid_feed"]     # Today's energy fed into grid
-    pvdata["day_usage"] = data["day_usage"]             # Today's total energy consumption
-    pvdata["day_usage_self"] = data["day_usage_self"]   # Today's energy consumption originating from own PV or battery (i.e. not grid)
-    pvdata["day_total"] = data["day_total"]             # Today's total energy production (PV + battery discharge)
+        data = api.query_usage_data(station_id, "daysummary")
+        pvdata["day_grid_load"] = { "value": data["day_grid_load"], "unit": "kWh" }     # Today's energy loaded from grid
+        pvdata["day_grid_feed"] = { "value": data["day_grid_feed"], "unit": "kWh" }     # Today's energy fed into grid
+        pvdata["day_usage"] = { "value": data["day_usage"], "unit": "kWh" }             # Today's total energy consumption
+        pvdata["day_usage_self"] = { "value": data["day_usage_self"], "unit": "kWh" }   # Today's energy consumption originating from own PV or battery (i.e. not grid)
+        pvdata["day_total"] = { "value": data["day_total"], "unit": "kWh" }             # Today's total energy production (PV + battery discharge)
 
-    # calculate autarky rate
-    autarky_rate = 100 * pvdata["day_usage_self"]["value"] / pvdata["day_usage"]["value"] if pvdata["day_usage"]["value"]>0 else 0
-    pvdata["day_autarky_rate"] = { "value": "{:.1f}".format(autarky_rate), "unit": "%" }    # Today's independance rate from grid power
-    self_usage_rate = 100 * pvdata["day_usage_self"]["value"] / pvdata["day_total"]["value"] if pvdata["day_total"]["value"]>0 else 0
-    pvdata["day_self_usage_rate"] = { "value": "{:.1f}".format(self_usage_rate), "unit": "%" }    # Ratio of self used energy (vs. fed into grid)
+        # calculate autarky rate
+        autarky_rate = 100 * pvdata["day_usage_self"]["value"] / pvdata["day_usage"]["value"] if pvdata["day_usage"]["value"]>0 else 0
+        pvdata["day_autarky_rate"] = { "value": "{:.1f}".format(autarky_rate), "unit": "%" }    # Today's independance rate from grid power
+        self_usage_rate = 100 * pvdata["day_usage_self"]["value"] / pvdata["day_total"]["value"] if pvdata["day_total"]["value"]>0 else 0
+        pvdata["day_self_usage_rate"] = { "value": "{:.1f}".format(self_usage_rate), "unit": "%" }    # Ratio of self used energy (vs. fed into grid)
+    except:
+        logging.error( "Error while retrieving PC data" )
 
     return pvdata
 
